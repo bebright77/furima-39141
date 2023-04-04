adeca57adb3d80bcc28e10e713987e7347f64967
bebright77 <aki.tomita.photo@gmail.com>
2023-04-04 16:18:10 +0900

    出品ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    
+    #@items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
+  
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_price_id, :sender_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
