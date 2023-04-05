cbd790c35bced143000320c5ab3653c3907408a6
bebright77 <aki.tomita.photo@gmail.com>
2023-04-05 14:51:16 +0900

    テストコードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :Condition
+  belongs_to :shipping_price
+  belongs_to :days_to_ship
+  belongs_to :sender  
+
   validates :image,             presence: true
   validates :item_name,         presence: true
   validates :explanation,       presence: true
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,27 +27,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it "カテゴリーは「---」が選択されているときは出品できない" do
-        @item.category_id = ''
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end  
       it "商品の状態は「---」が選択されているときは出品できない" do
-        @item.condition_id = ''
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it "送料がは「---」が選択されているときは出品できない" do
-        @item.shipping_price_id = ''
+        @item.shipping_price_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping price can't be blank")
       end
       it "発送元は「---」が選択されているときは出品できない" do
-        @item.sender_id = ''
+        @item.sender_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Sender can't be blank")
       end
       it "発送までの日数は「---」が選択されているときは出品できない" do
-        @item.days_to_ship_id = ''
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
