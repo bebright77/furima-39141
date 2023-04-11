04b8ee4f96c1dde00053547c76ad81d0bab5ac55
bebright77 <aki.tomita.photo@gmail.com>
2023-04-11 18:57:18 +0900

    テストコードの作成

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+
+    postal_code {"123-1234"}
+    sender_id {3}
+    municipality {"千代田区"}
+    address{"テスト12"}
+    phone_number {"09012341234"}
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
